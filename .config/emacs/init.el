39eb1d93ca1b6a32c01aa25b7d3bcb1882a78d89
Alexander Castillo <98887867+alcastic@users.noreply.github.com>
2022-07-18 23:09:09 -0400

    Update init.el

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,60 +1,70 @@
+;;; init.el  --- initialization file for Emacs
+;;; Commentary:
+;; Emacs Startup File --- initialization for Emacs
+
 ;;; Code:
+;; --- configure user-package - start =====================================================
 (require 'package) ; activate packages
 (package-initialize) ; activate all the packages (in particular autoloads)
-(add-to-list 'package-archives '("melpa" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/"))
 
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
-
 (require 'use-package)
-
 ;; --- configure user-package - end -------------------------------------------------------
 
 
-;; === configure general emacs variables - start ==========================================
-
-; Move customization variables to a separate file and load it
-(setq custom-file (locate-user-emacs-file "custom-vars.el"))
-(load custom-file 'noerror 'nomessage)
-
-(setq
- visible-bell t ; Visible signal (instead of sound) for non allow operations.
- use-dialog-box nil) ; Don't pop up UI dialogs when prompting.
-;; --- configure general emacs variables - end --------------------------------------------
-
-
-;; === configure ninor modes - start ======================================================
-;(menu-bar-mode -1)
+;; === general emacs configuration - start ================================================
+(menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (global-display-line-numbers-mode 1)
 (hl-line-mode 1)
 (blink-cursor-mode 1)
 (recentf-mode 1)
-;; --- configure minor modes - end --------------------------------------------------------
+
+; move customization variables to a separate file and load it
+(setq custom-file (locate-user-emacs-file "custom-vars.el"))
+(load custom-file 'noerror 'nomessage)
+
+(setq visible-bell t
+      use-dialog-box nil)
+;; --- general emacs configuration - end --------------------------------------------------
 
 
-;; === configure emacs theme - start ======================================================
-(setq modus-themes-mode-line '(accented borderless))
-(load-theme 'modus-vivendi t)
-;; --- Configure emacs theme - end --------------------------------------------------------
-
-;; === configure which-key theme - start ==================================================
+;; === emacs util packages - start ========================================================
+; minibuffer comand bind comletition
 (use-package which-key
   :ensure t
   :config
   (which-key-mode))
-;; --- configure which-key theme - end ----------------------------------------------------
 
-(use-package lsp-mode
+; counsel flexible tools for minibuffer completion in Emacs. Its includes:
+; - ivy: generic completion mechanism for Emacs.
+; - counsel: a collection of ivy-enhanced versions of common Emacs commands.
+; - swiper: ivy-enhanced alternative to Isearch.
+(use-package counsel
   :ensure t
-  :bind(:map lsp-mode-map
-	     ("C-c d" . lsp-describe-thing-at-point)
-	     ("C-c a" . lsp-executepcode-action)
-	     ("C-c l" . lsp-command-map))
   :config
-  (lsp-enable-which-key-integration t))
+  (ivy-mode 1)
+  (bind-key "C-c C-r" 'ivy-resume))
+(global-set-key (kbd "C-s") 'swiper-isearch)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "M-y") 'counsel-yank-pop)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "<f2> j") 'counsel-set-variable)
+(global-set-key (kbd "C-x b") 'ivy-switch-buffer)
+(global-set-key (kbd "C-c v") 'ivy-push-view)
+(global-set-key (kbd "C-c V") 'ivy-pop-view)
 
+; text completition framework
 (use-package company
   :ensure t
   :hook ((emacs-lisp-mode . (lambda ()
@@ -65,11 +75,24 @@
   (setq company-idle-delay 0.1
 	company-minimum-prefix-length 1))
 
-;; === configure lsp for golang - start ===================================================
-;; It requires installed golang installed and next dependencies:
-;;   $ go install golang.org/x/tools/gopls@latest
-;;   $ go install golang.org/x/tools/cmd/goimports@latest
-;;; Go
+; syntax checking
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+; client for language server protocol. lsp-mode provides integration for company and flycheck
+(use-package lsp-mode
+  :ensure t
+  :bind(:map lsp-mode-map
+	     ("C-c d" . lsp-describe-thing-at-point)
+	     ("C-c a" . lsp-executepcode-action)
+	     ("C-c l" . lsp-command-map))
+  :config
+  (lsp-enable-which-key-integration t))
+;; --- emacs util packages - start --------------------------------------------------------
+
+
+;;; === LSP => Go =========================================================================
 (use-package go-mode
   :ensure t
   :hook ((go-mode . lsp-deferred)
@@ -78,7 +101,8 @@
 	      ("<f6>"  . gofmt))
   :config
   (require 'lsp-go)
-  (setq lsp-go-analyses ;; for more details visit: https://github.com/golang/tools/blob/master/gopls/doc/analyzers.md
+  ;; https://github.com/golang/tools/blob/master/gopls/doc/analyzers.md
+  (setq lsp-go-analyses
 	'((fieldalignment . t)
 	  (nilness        . t)
 	  (unusedwrite    . t)
@@ -86,7 +110,32 @@
   ;; GOPATH/bin
   (add-to-list 'exec-path "~/go/bin")
   (setq gofmt-command "goimports"))
-;; --- configure lsp for golang - end -----------------------------------------------------
+;; --- LSP => Go -------------------------------------------------------------------------
+
+
+;; === configure emacs aparience - start ==================================================
+; all-the-icons requires to execute "M-x all-the-icons-install-fonts" after first installation
+(use-package all-the-icons
+  :ensure t)
+
+(use-package neotree
+  :ensure t)
+(global-set-key [f8] 'neotree-toggle)
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t)           ; other great theme: doom-ayu-dark
+  (doom-themes-visual-bell-config)   ; enable flashing mode-line on errors
+  (doom-themes-org-config))          ; corrects (and improves) org-mode's native fontification.
 
 (provide 'init)
 ;;; init.el ends here
